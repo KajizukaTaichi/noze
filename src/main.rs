e2397c44074fd6506fbd8840c8776d800cbef1a8
梶塚太智 <kajizukataichi@outlook.jp>
2024-08-31 20:56:18 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -238,6 +238,9 @@ fn noze(source: String, debug: bool) {
     // プリプロセッサなのぜ
     while pc < lines.len() {
         let code = lines[pc].trim();
+        pc += 1;
+
+        //　空白行は無視するのぜ
         if code.is_empty() {
             continue;
         }
@@ -257,7 +260,6 @@ fn noze(source: String, debug: bool) {
             eprintln!("エラー！文の終端には「のぜ」を付ける必要があるのぜ");
             return;
         }
-        pc += 1;
     }
 
     let mut call_stack: Vec<usize> = Vec::new();
@@ -266,6 +268,9 @@ fn noze(source: String, debug: bool) {
 
     while pc < lines.len() {
         let code = lines[pc].trim();
+        pc += 1;
+
+        //　空白行は無視するのぜ
         if code.is_empty() {
             continue;
         }
@@ -296,214 +301,207 @@ fn noze(source: String, debug: bool) {
             input("Enterキーを押してデバック実行継続");
         }
 
-        if code.ends_with("のぜ") {
-            let code = code.replace("のぜ", "");
-            if code.ends_with("する") {
-                let code = code.replace("する", "");
+        let code = code.replace("のぜ", "");
+        if code.ends_with("する") {
+            let code = code.replace("する", "");
 
-                // この文は定義するのぜ？
-                let (name, code) = if code.contains("は") {
-                    let code: Vec<&str> = code.split("は").collect();
-                    (
-                        Some(code[0..code.len() - 1].join("は")),
-                        code[code.len() - 1].to_string(),
-                    )
-                } else {
-                    (None, code.to_string())
-                };
+            // この文は定義するのぜ？
+            let (name, code) = if code.contains("は") {
+                let code: Vec<&str> = code.split("は").collect();
+                (
+                    Some(code[0..code.len() - 1].join("は")),
+                    code[code.len() - 1].to_string(),
+                )
+            } else {
+                (None, code.to_string())
+            };
 
-                // 命令名と引数を区切るのぜ
-                let code: Vec<&str> = code.split("を").collect();
-                let result: Type = if code.len() > 1 {
-                    let (order, args): (String, Vec<Type>) = (
-                        code[code.len() - 1].to_string(),
-                        code[0..code.len() - 1]
-                            .join("を")
-                            .split("と")
-                            .into_iter()
-                            .map(|s| Type::parse(s.to_string(), memory))
+            // 命令名と引数を区切るのぜ
+            let code: Vec<&str> = code.split("を").collect();
+            let result: Type = if code.len() > 1 {
+                let (order, args): (String, Vec<Type>) = (
+                    code[code.len() - 1].to_string(),
+                    code[0..code.len() - 1]
+                        .join("を")
+                        .split("と")
+                        .into_iter()
+                        .map(|s| Type::parse(s.to_string(), memory))
+                        .collect(),
+                );
+
+                match order.as_str() {
+                    "足し算" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        let mut result: f64 = args[0];
+                        for i in args[1..args.len()].to_vec().iter() {
+                            result += i;
+                        }
+                        Type::Number(result)
+                    }
+                    "引き算" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        let mut result: f64 = args[0];
+                        for i in args[1..args.len()].to_vec().iter() {
+                            result -= i;
+                        }
+                        Type::Number(result)
+                    }
+                    "掛け算" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        let mut result: f64 = args[0];
+                        for i in args[1..args.len()].to_vec().iter() {
+                            result *= i;
+                        }
+                        Type::Number(result)
+                    }
+                    "割り算" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        let mut result: f64 = args[0];
+                        for i in args[1..args.len()].to_vec().iter() {
+                            result /= i;
+                        }
+                        Type::Number(result)
+                    }
+                    "余剰" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        let mut result: f64 = args[0];
+                        for i in args[1..args.len()].to_vec().iter() {
+                            result %= i;
+                        }
+                        Type::Number(result)
+                    }
+                    "結合" => {
+                        let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
+                        Type::String(args.join(""))
+                    }
+                    "検索" => {
+                        let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
+                        Type::Bool(args[0].contains(&args[1]))
+                    }
+                    "ファイル読み込み" => Type::String(
+                        read_to_string(Path::new(&args[0].get_string())).unwrap_or_default(),
+                    ),
+                    "置換" => {
+                        let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
+                        Type::String(args[0].replace(&args[1], &args[2]))
+                    }
+                    "等しいか比較" => {
+                        let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
+                        Type::Bool(args.windows(2).all(|window| window[0] == window[1]))
+                    }
+                    "大きいか比較" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        Type::Bool(args.windows(2).all(|window| window[0] > window[1]))
+                    }
+                    "小さいか比較" => {
+                        let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
+                        Type::Bool(args.windows(2).all(|window| window[0] < window[1]))
+                    }
+                    "論理否定" => Type::Bool(!args[0].get_bool()),
+                    "論理積" => {
+                        let args: Vec<bool> = args.iter().map(|i| i.get_bool()).collect();
+                        Type::Bool(args.iter().all(|&x| x))
+                    }
+                    "論理和" => {
+                        let args: Vec<bool> = args.iter().map(|i| i.get_bool()).collect();
+                        Type::Bool(args.iter().any(|&x| x))
+                    }
+                    "配列に" => Type::Array(args),
+                    "配列の要素の取得" => {
+                        args[0].get_array()[args[1].get_number() as usize].clone()
+                    }
+                    "配列の要素の削除" => {
+                        let mut array = args[0].get_array();
+                        array.remove(args[1].get_number() as usize);
+                        Type::Array(array)
+                    }
+                    "表示" => {
+                        let text = args
+                            .iter()
+                            .map(|i| i.get_string())
+                            .collect::<Vec<String>>()
+                            .join(" ");
+                        if debug {
+                            output += &format!("{text} ");
+                        } else {
+                            println!("{text}")
+                        };
+                        Type::None
+                    }
+                    "移動" => {
+                        pc = args[0].get_number() as usize - 1;
+                        Type::None
+                    }
+                    "条件付きで移動" => {
+                        if args[1].get_bool() {
+                            pc = args[0].get_number() as usize - 1
+                        }
+                        Type::None
+                    }
+                    "呼び出し" => {
+                        call_stack.push(pc);
+                        pc = args[0].get_number() as usize - 1;
+                        Type::None
+                    }
+                    "評価" => Type::parse(args[0].get_string(), memory),
+                    "削除" => {
+                        memory.remove(&args[0].get_string());
+                        Type::None
+                    }
+                    "現在ディレクトリに" => {
+                        env::set_current_dir(Path::new(&args[0].get_string())).unwrap();
+                        Type::None
+                    }
+                    "入力待ち" => Type::String(input(&format!("{}", args[0].get_string()))),
+                    other => {
+                        eprintln!("エラー！定義されてない命令なのぜ：{}", other);
+                        return;
+                    }
+                }
+            } else {
+                match code[0] {
+                    "終了" => exit(0),
+                    "帰還" => {
+                        pc = if let Some(i) = call_stack.pop() {
+                            i
+                        } else {
+                            eprintln!("エラー！呼び出しスタックが空なのぜ");
+                            return;
+                        };
+                        Type::None
+                    }
+                    "アイテム一覧の取得" => Type::Array(
+                        fs::read_dir(".")
+                            .unwrap()
+                            .filter_map(|entry| {
+                                entry
+                                    .ok()
+                                    .and_then(|e| e.file_name().into_string().ok())
+                                    .map(Type::String)
+                            })
                             .collect(),
-                    );
-
-                    match order.as_str() {
-                        "足し算" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            let mut result: f64 = args[0];
-                            for i in args[1..args.len()].to_vec().iter() {
-                                result += i;
-                            }
-                            Type::Number(result)
-                        }
-                        "引き算" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            let mut result: f64 = args[0];
-                            for i in args[1..args.len()].to_vec().iter() {
-                                result -= i;
-                            }
-                            Type::Number(result)
-                        }
-                        "掛け算" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            let mut result: f64 = args[0];
-                            for i in args[1..args.len()].to_vec().iter() {
-                                result *= i;
-                            }
-                            Type::Number(result)
-                        }
-                        "割り算" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            let mut result: f64 = args[0];
-                            for i in args[1..args.len()].to_vec().iter() {
-                                result /= i;
-                            }
-                            Type::Number(result)
-                        }
-                        "余剰" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            let mut result: f64 = args[0];
-                            for i in args[1..args.len()].to_vec().iter() {
-                                result %= i;
-                            }
-                            Type::Number(result)
-                        }
-                        "結合" => {
-                            let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
-                            Type::String(args.join(""))
-                        }
-                        "検索" => {
-                            let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
-                            Type::Bool(args[0].contains(&args[1]))
-                        }
-                        "ファイル読み込み" => Type::String(
-                            read_to_string(Path::new(&args[0].get_string())).unwrap_or_default(),
-                        ),
-                        "置換" => {
-                            let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
-                            Type::String(args[0].replace(&args[1], &args[2]))
-                        }
-                        "等しいか比較" => {
-                            let args: Vec<String> = args.iter().map(|i| i.get_string()).collect();
-                            Type::Bool(args.windows(2).all(|window| window[0] == window[1]))
-                        }
-                        "大きいか比較" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            Type::Bool(args.windows(2).all(|window| window[0] > window[1]))
-                        }
-                        "小さいか比較" => {
-                            let args: Vec<f64> = args.iter().map(|i| i.get_number()).collect();
-                            Type::Bool(args.windows(2).all(|window| window[0] < window[1]))
-                        }
-                        "論理否定" => Type::Bool(!args[0].get_bool()),
-                        "論理積" => {
-                            let args: Vec<bool> = args.iter().map(|i| i.get_bool()).collect();
-                            Type::Bool(args.iter().all(|&x| x))
-                        }
-                        "論理和" => {
-                            let args: Vec<bool> = args.iter().map(|i| i.get_bool()).collect();
-                            Type::Bool(args.iter().any(|&x| x))
-                        }
-                        "配列に" => Type::Array(args),
-                        "配列の要素の取得" => {
-                            args[0].get_array()[args[1].get_number() as usize].clone()
-                        }
-                        "配列の要素の削除" => {
-                            let mut array = args[0].get_array();
-                            array.remove(args[1].get_number() as usize);
-                            Type::Array(array)
-                        }
-                        "表示" => {
-                            let text = args
-                                .iter()
-                                .map(|i| i.get_string())
-                                .collect::<Vec<String>>()
-                                .join(" ");
-                            if debug {
-                                output += &format!("{text} ");
-                            } else {
-                                println!("{text}")
-                            };
-                            Type::None
-                        }
-                        "移動" => {
-                            pc = args[0].get_number() as usize - 1;
-                            Type::None
-                        }
-                        "条件付きで移動" => {
-                            if args[1].get_bool() {
-                                pc = args[0].get_number() as usize - 1
-                            }
-                            Type::None
-                        }
-                        "呼び出し" => {
-                            call_stack.push(pc);
-                            pc = args[0].get_number() as usize - 1;
-                            Type::None
-                        }
-                        "評価" => Type::parse(args[0].get_string(), memory),
-                        "削除" => {
-                            memory.remove(&args[0].get_string());
-                            Type::None
-                        }
-                        "現在ディレクトリに" => {
-                            env::set_current_dir(Path::new(&args[0].get_string())).unwrap();
-                            Type::None
-                        }
-                        "入力待ち" => Type::String(input(&format!("{}", args[0].get_string()))),
-                        other => {
-                            eprintln!("エラー！定義されてない命令なのぜ：{}", other);
-                            return;
-                        }
+                    ),
+                    "現在ディレクトリの取得" => {
+                        Type::String(env::current_dir().unwrap().to_str().unwrap().to_string())
                     }
-                } else {
-                    match code[0] {
-                        "終了" => exit(0),
-                        "帰還" => {
-                            pc = if let Some(i) = call_stack.pop() {
-                                i
-                            } else {
-                                eprintln!("エラー！呼び出しスタックが空なのぜ");
-                                return;
-                            };
-                            Type::None
-                        }
-                        "アイテム一覧の取得" => Type::Array(
-                            fs::read_dir(".")
-                                .unwrap()
-                                .filter_map(|entry| {
-                                    entry
-                                        .ok()
-                                        .and_then(|e| e.file_name().into_string().ok())
-                                        .map(Type::String)
-                                })
-                                .collect(),
-                        ),
-                        "現在ディレクトリの取得" => {
-                            Type::String(env::current_dir().unwrap().to_str().unwrap().to_string())
-                        }
-                        other => {
-                            eprintln!("エラー！定義されてない命令なのぜ：{}", other);
-                            return;
-                        }
+                    other => {
+                        eprintln!("エラー！定義されてない命令なのぜ：{}", other);
+                        return;
                     }
-                };
+                }
+            };
 
-                // 結果を変数に代入するのぜ
-                if let Some(name) = name {
-                    memory.insert(name, result);
-                }
-            } else if code.ends_with("な") {
-                // リテラルで定義するのぜ
-                if code.contains("は") {
-                    let code: Vec<&str> = code.split("は").collect();
-                    let value = Type::parse(code[1].replace("な", "").trim().to_string(), memory);
-                    memory.insert(code[0].to_string(), value);
-                }
+            // 結果を変数に代入するのぜ
+            if let Some(name) = name {
+                memory.insert(name, result);
             }
-        } else {
-            eprintln!("エラー！文の終端には「のぜ」を付ける必要があるのぜ");
+        } else if code.ends_with("な") {
+            // リテラルで定義するのぜ
+            if code.contains("は") {
+                let code: Vec<&str> = code.split("は").collect();
+                let value = Type::parse(code[1].replace("な", "").trim().to_string(), memory);
+                memory.insert(code[0].to_string(), value);
+            }
         }
-
-        // プログラムカウンタを加算するのぜ
-        pc += 1;
     }
 }
